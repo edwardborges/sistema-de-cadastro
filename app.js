854f1b400329b3118705e00a0ff2e9c1986b0edb
edwardborges <eduardoluizb@yahoo.com.br>
2022-10-19 15:38:13 -0300

    teste para voltar a versão no console.log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o gerenciamento de jogos")
 console.log(" Aplicação está adcionando um bando de dados")
 console.log(" Alteração de commit sem ser no vscode local")
+console.log(" teste para voltar a versão anterior")
